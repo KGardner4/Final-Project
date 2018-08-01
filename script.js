95776301a0ed0888743ff698aebecd3db264bd6e
Kayla <kmgardner4@cps.edu>
2018-08-01 15:04:23 -0500

    finished

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 
-$(document).ready (function() }
-<script>
- 
+$(document).ready(function() {
+	
+	
+	$("#q1true").click(function() {
+		console.log("hi");
+		$("#q1result").html("Diligent");
+	});
+		$("#q1false").click(function() {
+		console.log("hi");
+		$("#q1result").html("hardworking");
+	});
+		$("#q2true").click(function() {
+		console.log("hi");
+		$("#q2result").html("childish");
+	});
+		$("#q2false").click(function() {
+		console.log("hi");
+		$("#q2result").html("focus");
+	});
+		$("#q3true").click(function() {
+		console.log("hi");
+		$("#q3result").html("sensitive");
+	});
+		$("#q3false").click(function() {
+		console.log("hi");
+		$("#q3result").html("cold-hearted");
+	});
 
+	
 
-</script>
-
-
-}};
+});
