60d008477f978daf286f5e2f77fe43eb2efb41be
DePaul <CONF0178@depaul.edu>
2018-08-02 11:37:41 -0500

    adding script

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
-$(document).ready (function() }
-<script>
- 
+$(document).ready (function() {
 
+	var search_word = "dress";
 
-</script>
+	$.ajax({url: "https://api.qloo.com/suggestions?qloos.filter.category=fashion/brands&pageSize=30&page=1&query=" + search_word, success: function(result){
+        //$("#div1").html(result);
+		console.log(result);
+    }});
+	
+	$.ajax({url: "https://api.qloo.com/qloos/FE5AACD4-7F2A-47CF-B622-4715D3C592E7/similar?pageSize=5", success: function(result){
+        //$("#div1").html(result);
+		console.log(result);
+    }});
 
-
-}};
+});
