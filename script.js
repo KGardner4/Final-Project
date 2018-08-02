918775a705483d2e0a147ae7963934d4690359b4
Kayla <kmgardner4@cps.edu>
2018-08-02 15:46:18 -0500

    updates

--- script.js
+++ script.js
@@ -5,38 +5,65 @@ $(document).ready(function() {
 		console.log("hi");
 		$("#q1result").html("Diligent");
 	});
-		$("#q1false").click(function() {
+		
+	$("#q1false").click(function() {
 		console.log("hi");
 		$("#q1result").html("hardworking");
 	});
-		$("#q2true").click(function() {
+		
+	$("#q2true").click(function() {
 		console.log("hi");
 		$("#q2result").html("childish");
 	});
-		$("#q2false").click(function() {
+		
+	$("#q2false").click(function() {
 		console.log("hi");
 		$("#q2result").html("focus");
 	});
-		$("#q3true").click(function() {
+	
+	$("#q3true").click(function() {
 		console.log("hi");
 		$("#q3result").html("sensitive");
 	});
-		$("#q3false").click(function() {
+	
+	$("#q3false").click(function() {
 		console.log("hi");
 		$("#q3result").html("cold-hearted");
+	})
+	
+	$("button").click(function(){
+		//$("input:text").val("Search");
+		
+		var search_word = $("#search_input").val();
+		console.log(search_word);
+		
+		$.ajax({url: "https://api.qloo.com/suggestions?qloos.filter.category=fashion/brands&pageSize=30&page=1&query=" + search_word, success: function(result){
+			console.log(result);
+			var id = result["body"]["suggestions"][0]["id"];
+			console.log(id);
+			
+			$.ajax({url: "https://api.qloo.com/" + id + "/similar?pageSize=5", success: function(result){
+			console.log(result);
+			
+				var arr = result["body"]["qloos"];
+				for (var i=0; i<arr.length; i++) {
+					var item = result["body"]["qloos"][i];
+					var item_title = item["title"];
+					var item_disamb = item["disambiguation"];
+					console.log(item_title);
+					
+					// display result in HTML
+					$("#fasion").html("");
+					$("#fashion").append("<h4>" +item_title + "</h4>");
+					$("#fashion").append("<h5>" +item_disamb + "</h5>");
+				}
+			}});
+			
+		}});
+		
+		
 	});
-
-	var search_word = "dress";
-
-	$.ajax({url: "https://api.qloo.com/suggestions?qloos.filter.category=fashion/brands&pageSize=30&page=1&query=" + search_word, success: function(result){
-        //$("#div1").html(result);
-		console.log(result);
-    }});
 	
-	$.ajax({url: "https://api.qloo.com/qloos/FE5AACD4-7F2A-47CF-B622-4715D3C592E7/similar?pageSize=5", success: function(result){
-        //$("#div1").html(result);
-		console.log(result);
-    }});
 	
-
+	
 });
